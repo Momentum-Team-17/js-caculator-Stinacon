f081e3d3a45f634011231dfd976aa0d9c94ba4ff
Christina <stinacon@gmail.com>
2023-02-01 08:38:52 -0500

    tried 'click' event-listener in for-loop on number buttons with console.log test; not working so far

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
 // let numButton = document.querySelectorAll('.button.num');
 // console.log(numButton);
 
-let sevenButton = document.querySelector('#seven');
-console.log(sevenButton);
+// let sevenButton = document.querySelector('#seven');
+// console.log(sevenButton);
 
-sevenButton.addEventListener('click', function (event) {
-    console.log(event.target.innerText);
-}
-)
-
-// numButton.addEventListener('click', function () {
+// sevenButton.addEventListener('click', function (event) {
+//     console.log(event.target.innerText);
 
 // }
 // )
-// console.log()
+
+//need to figure out how to loop the above event for all number buttons -- also how to make the number appear in the other box!!!
+let numButtonArray = document.querySelectorAll('button.num');
+
+for (let numButton of numButtonArray) {
+    numButton.addEventListener('click', function (event) {
+        console.log(event.target.innerText)
+    }
+    )
+}
