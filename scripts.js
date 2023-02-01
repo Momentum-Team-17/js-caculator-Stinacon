e995760ce5164a343d01feda4e4d7dd541bd82b7
Christina <stinacon@gmail.com>
2023-01-31 19:30:33 -0500

    wrote and tested succesful JS for console log to print inner text when seven button is clicked; next will be try to loop

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// let numButton = document.querySelectorAll('.button.num');
+// console.log(numButton);
+
+let sevenButton = document.querySelector('#seven');
+console.log(sevenButton);
+
+sevenButton.addEventListener('click', function (event) {
+    console.log(event.target.innerText);
+}
+)
+
+// numButton.addEventListener('click', function () {
+
+// }
+// )
+// console.log()
