3b05a0abd534d248dbeb7a25c2e24a0ce8c3f2da
Christina <stinacon@gmail.com>
2023-02-01 11:36:02 -0500

    added JS to make button text appear in diplay bar when clicked

--- scripts.js
+++ scripts.js
@@ -11,11 +11,34 @@
 // )
 
 //need to figure out how to loop the above event for all number buttons -- also how to make the number appear in the other box!!!
-let numButtonArray = document.querySelectorAll('.button.num');
-
-for (let numButton of numButtonArray) {
-    numButton.addEventListener('click', function (event) {
-        console.log(event.target.innerText)
+let buttonArray = document.querySelectorAll('.button');
+let display = document.querySelector('#result-space');
+const filledDisplay = []
+for (let button of buttonArray) {
+    button.addEventListener('click', function (event) {
+        // console.log(event.target.innerText)
+        let buttonText = event.target.innerText;
+        filledDisplay.push(buttonText);
+        display.innerText = filledDisplay.join("")
     }
     )
 }
+
+
+
+
+//how do i "capture" the inner text of the click event to move to the target, "result-space"?
+//something about appendChild(text)?
+
+//^selects where I want the inner text of my number button to go???
+//let existingElement = document.querySelector('.empty-div')
+// First select where you want to place the new content
+
+//let newElement = document.createElement('div')
+// Create the new element
+//let text = document.createTextNode('Hello there!')
+// Create some text
+//newElement.appendChild(text)
+// Add text to new element
+//existingElement.appendChild(newElement)
+// Add that element to the DOM
